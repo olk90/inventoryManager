bcf52dc665983a8ac4a7bcd51021378c780051ca
Oliver Köhler <olk.git@posteo.eu>
2021-08-01 21:19:43 +0200

    moved main controller logic to WorkspaceController.kt

--- src/main/kotlin/de/olk90/inventorymanager/logic/controller/WorkspaceController.kt
+++ src/main/kotlin/de/olk90/inventorymanager/logic/controller/WorkspaceController.kt
@@ -1,4 +1,37 @@
 package de.olk90.inventorymanager.logic.controller
 
+import de.olk90.inventorymanager.logic.Config
+import javafx.fxml.FXML
+import javafx.fxml.FXMLLoader
+import javafx.scene.Parent
+import javafx.scene.Scene
+import javafx.scene.layout.BorderPane
+import javafx.stage.Stage
 
-class WorkspaceController
+class WorkspaceController {
+
+    @FXML
+    lateinit var mainView: BorderPane
+
+
+    fun openNewDatabase() {
+        val resource = javaClass.classLoader.getResource("fxml/databaseFragment.fxml")
+        val fragment = FXMLLoader.load<Parent>(resource, Config.getResourceBundle())
+        val secondaryScene = Scene(fragment, 300.0, 170.0)
+
+        val newWindow = Stage()
+        newWindow.scene = secondaryScene
+        newWindow.show()
+    }
+
+    fun openPersonsView() {
+        val resource = javaClass.classLoader.getResource("fxml/personsView.fxml")
+        mainView.center = FXMLLoader.load<Parent>(resource, Config.getResourceBundle())
+    }
+
+    fun openInventoryView() {
+        val resource = javaClass.classLoader.getResource("fxml/inventoryView.fxml")
+        mainView.center = FXMLLoader.load<Parent>(resource, Config.getResourceBundle())
+    }
+
+}
