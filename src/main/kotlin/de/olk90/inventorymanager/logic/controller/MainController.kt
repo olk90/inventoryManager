27a7aa29d397a94718469f8e35db6046a2a01ca4
Oliver Köhler <olk.git@posteo.eu>
2021-08-01 16:52:13 +0200

    switching views on button click

--- src/main/kotlin/de/olk90/inventorymanager/logic/controller/MainController.kt
+++ src/main/kotlin/de/olk90/inventorymanager/logic/controller/MainController.kt
@@ -1,4 +1,27 @@
 package de.olk90.inventorymanager.logic.controller
 
+import javafx.fxml.FXML
+import javafx.fxml.FXMLLoader
+import javafx.scene.Parent
+import javafx.scene.layout.BorderPane
+
 class MainController {
+
+    @FXML
+    lateinit var mainView: BorderPane
+
+
+    fun openNewDatabase() {
+
+    }
+
+    fun openPersonsView() {
+        val resource = javaClass.classLoader.getResource("fxml/personsView.fxml")
+        mainView.center = FXMLLoader.load<Parent>(resource)
+    }
+
+    fun openInventoryView() {
+        val resource = javaClass.classLoader.getResource("fxml/inventoryView.fxml")
+        mainView.center = FXMLLoader.load<Parent>(resource)
+    }
 }
