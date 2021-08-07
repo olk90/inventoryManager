0a28f9cc186c6a8fb79bc679029944d0985c89f8
Oliver Köhler <olk.git@posteo.eu>
2021-08-08 00:05:50 +0200

    tweaked date picker combobox

--- src/main/kotlin/de/olk90/inventorymanager/logic/controller/InventoryController.kt
+++ src/main/kotlin/de/olk90/inventorymanager/logic/controller/InventoryController.kt
@@ -63,6 +63,7 @@ class InventoryController {
             Locale.getDefault(),
             "MMM/yyyy"
         )
+        yearMonthPicker.maxWidth = Double.MAX_VALUE
         editorGrid.add(yearMonthPicker, 1, 2)
     }
 
